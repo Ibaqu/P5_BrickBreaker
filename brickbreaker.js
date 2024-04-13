c50dcfe67ecd8416fe3104e1133a0a899c922ad3
Ibaqu <aquib.zt@gmail.com>
2024-04-13 07:43:41 -0400

    Add bricks with collision

--- brickbreaker.js
+++ brickbreaker.js
@@ -1,47 +1,80 @@
 // Random Ball Placement
 var xBall = Math.floor(Math.random() * 300) + 50;
-var yBall = 50;
+var yBall = 300;
 var xSpeed = (2, 7);
 var ySpeed = (-7, -2);
 var score = 0
 
-// Canvas
-function setup() {
-  createCanvas(400, 400);
+// Initialize brick array
+var bricks = [];
+for(let i=0; i<20; i++) {
+  bricks.push({
+    x: Math.random()*400,
+    y: 100 + 20*i,
+    width: 70,
+    height: 20,
+  });
 }
 
-//Background
-
 function draw() {
-
-  // Background
+  //Render Background
   background(0);
 
-  // Paddle
+  //Render Paddle
   fill('#ffffff');
   rect(mouseX, 375, 90, 15);
 
-  //Functions
+  //Render Bricks
+  for(let i=0; i<bricks.length; i++) {
+    fill('#d9c3f7');
+    rect(bricks[i].x, bricks[i].y, bricks[i].width, bricks[i].height);
+  }
+
+  //Render Ball
+  fill('#d9c3f7');
+  ellipse(xBall, yBall, 20, 20);
+
+  //Actions
   move();
-  display();
   bounce();
-  //resetBall();
   paddle();
+  checkCol();
+  removeBricks();
 
   //Score
   fill('#d9c3f7');
   textSize(24);
   text("Score: " + score, 10, 25);
 }
+
+function checkCol() {
+  for(let i=0; i<bricks.length; i++) {
+    if(xBall + 10 > bricks[i].x && xBall - 10 < bricks[i].x + bricks[i].width && yBall > bricks[i].y && yBall < bricks[i].y + bricks[i].height) {
+      ySpeed *= -1;
+      bricks.splice(i, 1);
+      score++;
+    }
+  }
+}
+
+function removeBricks() {
+  bricks = bricks.filter(function(brick) {
+    return brick.y < 500;
+  });
+}
+
+// Canvas
+function setup() {
+  createCanvas(400, 400);
+}
+
 // Ball Functions
 function move() {
   xBall += xSpeed;
   yBall += ySpeed;
 }
 
-
 function bounce() {
-
   if (xBall < 10 ||
     xBall > 400 - 10) {
     xSpeed *= -1;
